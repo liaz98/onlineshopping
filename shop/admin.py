2517542d793a422afa915bd286b9b54fe13f2fbb
Azizabonu <azibonu19@gmail.com>
2021-04-02 19:24:37 +0500

    Online-shopping

---
+++ shop/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import Category, Product
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug' : ('name',)}
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'price', 'available', 'created', 'updated']
+    list_filter = ['available', 'created', 'updated']
+    list_ediatable = ['available', 'price']
+    prepopulated_fields = {'slug':('name',)}
